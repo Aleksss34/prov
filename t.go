5ba0e96ee300c86dee3799e2e888ee0e0e509c9b
aleks <tremaskin.aleks@mail.ru>
2024-10-25 20:23:48 +0300

    site

--- t.go
+++ t.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+type Handler struct {
+	Name string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "name: %s, url: %v", h.Name, r.URL.String())
+}
 func main() {
-	fmt.Println()
+	testHandler := &Handler{Name: "test"}
+	http.Handle("/tests/", testHandler)
+	rootHandler := &Handler{Name: "root"}
+	http.Handle("/", rootHandler)
+	http.ListenAndServe(":8080", nil)
 }
